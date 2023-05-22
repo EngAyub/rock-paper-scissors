9dda179fd27fc9b72ec5c73ff4184b478a057ea0
ENG AYUUB <moztaphem9@gmal.com>
2023-05-22 22:02:52 +0300

    Rock paper scissors Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function playGame(play1, play2) {
+    if (play1 === play2) {
+      return "tie";
+    } else if (
+      (play1 === "rock" && play2 === "scissors") ||
+      (play1 === "paper" && play2 === "rock") ||
+      (play1 === "scissors" && play2 === "paper")
+    ) {
+      return "Player One win";
+    } else {
+      return "Player Two win";
+    }
+  }
+  console.log(playGame("rock", "paper"));
+  console.log(playGame("scissors", "paper")); 
+  console.log(playGame("rock", "rock"));     
+  console.log(playGame("scissors", "rock"));  
